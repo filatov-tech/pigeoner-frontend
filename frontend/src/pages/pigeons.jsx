06ac4b5672310e6d998dccaa84c8620193d8ce31
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-06-20 21:30:50 -0300

    Add pigeon filter on the page

--- frontend/src/pages/pigeons.jsx
+++ frontend/src/pages/pigeons.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import '../styles/pigeons.css';
+import PigeonFilterForm from "../components/UI/pigeonsTable/PigeonFilterForm";
 
 const Pigeons = () => {
     return (
-        <div>
-            <h1>Pigeons list</h1>
-        </div>
+        <Container>
+            <Row>
+                <div className="col-12">
+                    <PigeonFilterForm />
+                </div>
+            </Row>
+            <Row>
+                <Col>
+
+
+
+                    {/*<div className="table-responsive text-center">*/}
+                    {/*    <table id="pigeons-table" className="table table-striped table-hover">*/}
+                    {/*        <thead className="text-center pigeons-table-head">*/}
+                    {/*        <tr>*/}
+                    {/*            <th>Кольцо</th>*/}
+                    {/*            <th>Окрас</th>*/}
+                    {/*            <th>Пол</th>*/}
+                    {/*            <th>Родился</th>*/}
+                    {/*            <th>Возраст</th>*/}
+                    {/*            <th>Пара</th>*/}
+                    {/*            <th>Статус</th>*/}
+                    {/*            <th></th>*/}
+                    {/*        </tr>*/}
+                    {/*        </thead>*/}
+                    {/*        <tbody></tbody>*/}
+                    {/*    </table>*/}
+                    {/*</div>*/}
+                </Col>
+            </Row>
+        </Container>
+
     );
 };
 
