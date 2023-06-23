63c144039a74b5720aa9e50042dd248a26ca5b9e
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-06-23 15:50:13 -0300

    Add margin for page content

--- frontend/src/pages/layout.jsx
+++ frontend/src/pages/layout.jsx
@@ -7,7 +7,9 @@ const Layout = () => {
     return (
         <>
             <Header/>
-            <Outlet/>
+            <div className="page-body">
+                <Outlet/>
+            </div>
             <Footer/>
         </>
     );
