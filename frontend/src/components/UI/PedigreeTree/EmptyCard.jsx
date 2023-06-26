e9a6dba95a59bb6782f1c969cdc948ecb2f2f416
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-06-26 13:10:45 -0300

    Make AddButton displayed in center

--- frontend/src/components/UI/PedigreeTree/EmptyCard.jsx
+++ frontend/src/components/UI/PedigreeTree/EmptyCard.jsx
@@ -4,11 +4,11 @@ import AddButton from "../button/AddButton";
 const EmptyCard = ({pigeonStub}) => {
     return (
         <div className="pigeon-card card-empty">
-            <div className={`card no-border ${pigeonStub.cardSize}`}>
+            <div className={`card no-border ${pigeonStub.cardSize} d-flex`} style={{height: 100 + "%"}}>
                 <div className={`card-header no-border`}>
                     <h5 className="mb-0">Пустой слот</h5>
                 </div>
-                <div className="card-body" style={{paddingLeft: 25 + "%"}}>
+                <div className="card-body d-flex align-items-center justify-content-center">
                     {pigeonStub.parentId > 0 && <AddButton />}
                 </div>
             </div>
