2fd2b60a1bbbf96293a0aa6b03f52e6081025f76
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-07-05 12:46:48 -0300

    Change Flights section name

--- frontend/src/components/UI/Header.jsx
+++ frontend/src/components/UI/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
                     </Navbar.Brand>
                     <Nav className={"justify-content-end"} style={{fontSize: 1.5 + "rem"}}>
                         <Nav.Link href={"/pigeons"}>Голуби</Nav.Link>
-                        <Nav.Link href={"/flights"}>Зачеты</Nav.Link>
+                        <Nav.Link href={"/flights"}>Полеты</Nav.Link>
                         <Nav.Link href={"/dovecote"}>Голубятня</Nav.Link>
                         <Nav.Link href={"/diet"}>Питание</Nav.Link>
                     </Nav>
