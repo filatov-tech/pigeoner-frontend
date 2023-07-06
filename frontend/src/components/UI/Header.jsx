955c129a48f2aac7c955096da5267425f278c008
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-07-06 13:44:03 -0300

    Rebuild site header for a seamless transition

--- frontend/src/components/UI/Header.jsx
+++ frontend/src/components/UI/Header.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
 import {Container, Nav, Navbar} from "react-bootstrap";
 import logo from '../../images/pigeoner_logo.svg';
+import {Link, NavLink} from "react-router-dom";
 
 const Header = () => {
     return (
         <div>
-            <Navbar bg={"light"} variant={"light"}>
-                <Container>
-                    <Navbar.Brand href={"/home"}>
+            <nav className="navbar navbar-expand-lg custom-navbar">
+                <div className="container">
+                    <Link to={"/"}>
                         <img src={logo} alt="Pigeoner logo"/>
-                    </Navbar.Brand>
-                    <Nav className={"justify-content-end"} style={{fontSize: 1.5 + "rem"}}>
-                        <Nav.Link href={"/pigeons"}>Голуби</Nav.Link>
-                        <Nav.Link href={"/flights"}>Полеты</Nav.Link>
-                        <Nav.Link href={"/dovecote"}>Голубятня</Nav.Link>
-                        <Nav.Link href={"/diet"}>Питание</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
+                    </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <ul className="navbar-nav nav-underline">
+                            <li className="nav-item">
+                                <NavLink to={"/"} className="nav-link">Главная</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={"/pigeons"} className="nav-link">Голуби</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={"/flights"} className="nav-link">Полеты</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={"/dovecote"} className="nav-link">Голубятня</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={"/feeding"} className="nav-link">Питание</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 };
