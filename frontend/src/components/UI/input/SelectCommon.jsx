11f2e31198855679118e07dbe2d6dd4f2f7d79ff
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-09-17 09:24:47 -0300

    Add the error providing to the SelectCommon

--- frontend/src/components/UI/input/SelectCommon.jsx
+++ frontend/src/components/UI/input/SelectCommon.jsx
@@ -1,7 +1,8 @@
 import React, {useId} from 'react';
-import {createTheme, FormControl, InputLabel, MenuItem, Select, ThemeProvider} from "@mui/material";
+import {createTheme, FormControl, FormHelperText, InputLabel, MenuItem, Select, ThemeProvider} from "@mui/material";
+import {getHelperText} from "../../../util/utils";
 
-const SelectCommon = ({data, onChange, customStyle, withoutAny, variant, ...muiProps}) => {
+const SelectCommon = ({data, onChange, error, customStyle, withoutAny, variant, ...muiProps}) => {
     const selectId = useId();
 
     const theme = createTheme({
@@ -10,20 +11,24 @@ const SelectCommon = ({data, onChange, customStyle, withoutAny, variant, ...muiP
 
     return (
         <ThemeProvider theme={theme}>
-            <FormControl fullWidth variant={variant}>
+            <FormControl fullWidth variant={variant} error={error}>
                 {data.label && <InputLabel id={selectId}>{data.label}</InputLabel>}
                 <Select
                     labelId={selectId}
                     id={data.name}
                     value={data.value}
                     label={data.label}
-                    onChange={(e) => onChange(e.target.value)}
+                    onChange={(e) => {
+                        error && error.disable(data.name);
+                        onChange(e.target.value)
+                    }}
                     {...muiProps}
                 >
                     {!withoutAny && <MenuItem value={null}><em>не важно</em></MenuItem>}
                     {data.options.map(option =>
                         <MenuItem value={option.value} key={option.value}>{option.label}</MenuItem>)}
                 </Select>
+                <FormHelperText>{getHelperText(error)}</FormHelperText>
             </FormControl>
         </ThemeProvider>
 
