f03082b0ecdb97dde3edb73080977e3f2c59fbb7
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-10-06 12:18:59 -0300

    Add a precise distance field

--- frontend/src/components/UI/form/dialog/LaunchPointEditDialog.jsx
+++ frontend/src/components/UI/form/dialog/LaunchPointEditDialog.jsx
@@ -1,4 +1,4 @@
-import React, {forwardRef, useImperativeHandle, useState} from 'react';
+import React, {forwardRef, useEffect, useImperativeHandle, useState} from 'react';
 import {
     Button,
     DialogContent,
@@ -14,8 +14,9 @@ import {
 import {InfoOutlined} from "@mui/icons-material";
 import {getHelperText} from "../../../../util/utils";
 import {LAUNCH_POINTS_URL} from "../../input/Autocomplete/InputLaunchPointAutocompleteCreatable";
+import {KEEPER_URL, MAIN_KEEPER_URL} from "../../../../pages/pigeons"
 
-const emptyValue = {id: null ,name:"", distance: null, label: ""}
+const emptyValue = {id: null ,name:"", distance: null, mainKeeperPreciseDistance: null, label: ""}
 
 const LaunchPointEditDialog = (props, ref) => {
     const [open, setOpen] = useState(false);
@@ -23,8 +24,10 @@ const LaunchPointEditDialog = (props, ref) => {
     const [editMode, setEditMode] = useState(false);
 
     const [error, setError] = useState(null);
-    const [nameError, setNameError] = useState(null);
-    const [distanceError, setDistanceError] = useState(null);
+    const [errors, setErrors] = useState({});
+
+    const [keepers, setKeepers] = useState([]);
+    const [mainKeeper, setMainKeeper] = useState({});
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -39,7 +42,8 @@ const LaunchPointEditDialog = (props, ref) => {
             });
             if (response.ok) {
                 const created = await response.json();
-                cleanErrors();
+                setError(null);
+                setErrors({});
                 props.onChange(created);
                 if (props.onSubmit) {
                     props.onSubmit.forEach((callback) => callback());
@@ -47,8 +51,9 @@ const LaunchPointEditDialog = (props, ref) => {
             } else {
                 const apiError = await response.json();
                 setError(apiError);
-                setNameError(apiError.fields.name);
-                setDistanceError(apiError.fields.distance);
+                if (apiError.fields) {
+                    setErrors(apiError.fields)
+                }
                 return;
             }
         } catch (e) {
@@ -57,15 +62,39 @@ const LaunchPointEditDialog = (props, ref) => {
         handleClose()
     }
 
+    const fetchKeepers = async () => {
+        try {
+            const response = await fetch(KEEPER_URL);
+            if (response.ok) {
+                setKeepers(await response.json());
+            }
+        } catch (e) {
+            throw new Error("Ошибка при загрузке списка владельцев", e)
+        }
+    }
+
+    const fetchMainKeeper = async () => {
+        try {
+            const response = await fetch(MAIN_KEEPER_URL + "/withPreciseDistances");
+            if (response.ok) {
+                const mainKeeper = await response.json();
+                setMainKeeper(mainKeeper);
+                setValue({...value, mainKeeperPreciseDistance: mainKeeper.preciseDistances[value.id]})
+
+            }
+        } catch (e) {
+            throw new Error("Ошибка при загрузке основного владельца", e)
+        }
+    }
+
+    useEffect(() => {
+        fetchKeepers();
+        fetchMainKeeper();
+    }, []);
+
     const handleClose = () => {
         setValue(emptyValue);
         setOpen(false);
-    }
-
-    const cleanErrors = () => {
-        setError(null);
-        setNameError(null);
-        setDistanceError(null);
     }
 
     const openWithValue = (incomingValue) => {
@@ -80,7 +109,8 @@ const LaunchPointEditDialog = (props, ref) => {
             name: incomingValue.name,
             distance: incomingValue.distance,
             id: incomingValue.id,
-            label: incomingValue.label
+            label: incomingValue.label,
+            mainKeeperPreciseDistance: incomingValue.mainKeeperPreciseDistance
         })
         setOpen(true);
     }
@@ -96,8 +126,8 @@ const LaunchPointEditDialog = (props, ref) => {
                 <DialogTitle>{editMode ? "Редактирование точки выпуска" : "Новая точка выпуска"}</DialogTitle>
                 <DialogContent>Заполните информацию о точке выпуска голубей</DialogContent>
                 <form onSubmit={handleSubmit}>
-                    <Stack spacing={3} direction="row">
-                        <FormControl error={nameError}>
+                    <Stack spacing={3}>
+                        <FormControl error={errors.name}>
                             <FormLabel>Название точки</FormLabel>
                             <Input
                                 autoFocus
@@ -107,7 +137,9 @@ const LaunchPointEditDialog = (props, ref) => {
                                 size="lg"
                                 value={value.name}
                                 onChange={(e) => {
-                                    setNameError(null);
+                                    setErrors({
+                                        ...errors,
+                                        name: null})
                                     setValue({
                                         ...value,
                                         name: e.target.value
@@ -115,12 +147,12 @@ const LaunchPointEditDialog = (props, ref) => {
                                 }}
                             />
                             <FormHelperText>
-                                {nameError && <InfoOutlined/>}
-                                {getHelperText(nameError)}
+                                {errors.name && <InfoOutlined/>}
+                                {getHelperText(errors.name)}
                             </FormHelperText>
                         </FormControl>
-                        <FormControl error={distanceError}>
-                            <FormLabel>Дистанция, км</FormLabel>
+                        <FormControl error={errors.distance}>
+                            <FormLabel>Номинальная дистанция, км</FormLabel>
                             <Input
                                 id="distance"
                                 type="number"
@@ -128,7 +160,9 @@ const LaunchPointEditDialog = (props, ref) => {
                                 size="lg"
                                 value={value.distance}
                                 onChange={(e) => {
-                                    setDistanceError(null);
+                                    setErrors({
+                                        ...errors,
+                                        distance: null})
                                     setValue({
                                         ...value,
                                         distance: e.target.value
@@ -136,8 +170,33 @@ const LaunchPointEditDialog = (props, ref) => {
                                 }}
                             />
                             <FormHelperText>
-                                {distanceError && <InfoOutlined/>}
-                                {getHelperText(distanceError)}
+                                {errors.distance && <InfoOutlined/>}
+                                {getHelperText(errors.distance)}
+                            </FormHelperText>
+                        </FormControl>
+                        <FormControl error={errors.mainKeeperPreciseDistance}>
+                            <FormLabel>Точная дистанция, км</FormLabel>
+                            <Input
+                                id="mainKeeperPreciseDistance"
+                                type="number"
+                                slotProps={{
+                                    input: {
+                                        step: 0.001
+                                    }
+                                }}
+                                variant="outlined"
+                                size="lg"
+                                value={value.mainKeeperPreciseDistance}
+                                onChange={(e) => {
+                                    setValue({
+                                        ...value,
+                                        mainKeeperPreciseDistance: e.target.value
+                                    })
+                                }}
+                            />
+                            <FormHelperText>
+                                {errors.mainKeeperPreciseDistance && <InfoOutlined/>}
+                                {getHelperText(errors.mainKeeperPreciseDistance)}
                             </FormHelperText>
                         </FormControl>
                     </Stack>
