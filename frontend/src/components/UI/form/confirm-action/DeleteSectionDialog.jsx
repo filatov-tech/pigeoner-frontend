3e6330275ed8d9e90dd8e71f2171bb7c796f20c7
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-10-11 13:55:24 -0300

    Add DeleteSectionDialog.jsx
    Here the user must confirm his intention to delete the object

---
+++ frontend/src/components/UI/form/confirm-action/DeleteSectionDialog.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {Button, DialogContent, DialogTitle, Modal, ModalClose, ModalDialog, Stack} from "@mui/joy";
+import {DeleteForeverOutlined} from "@mui/icons-material";
+
+const DeleteSectionDialog = (props) => {
+
+    const text = {
+        NEST: "Вы пытаетесь удалить гнездо. Все голуби останутся в текущей секции, в разделе \"Вне гнезд\"",
+        ROOM: "Вы пытаетесь удалить секцию. Это приведет к удалению всех вложенных секций и гнезд. Все голуби переместяться в раздел \"Нераспределенные\"",
+        DOVECOTE: "Внимание! Удаление голубятни приведет к удалению всех вложенных секций и гнезд. Все голуби переместяться в раздел \"Нераспределенные\""
+    };
+    const title = {
+        NEST: "гнезда",
+        ROOM: "секции",
+        DOVECOTE: "голубятни"
+    }
+
+    return (
+        <Modal
+            aria-labelledby="modal-title"
+            aria-describedby="modal-desc"
+            open={props.open}
+            onClose={(e) => {
+                e.stopPropagation();
+                props.setOpen(false);
+            }}
+            sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+        >
+            <ModalDialog
+                color="danger"
+                layout="center"
+                size="md"
+                variant="outlined"
+            >
+                <ModalClose onClick={(e) => e.stopPropagation()} />
+                <DialogTitle>Удаление {title[props.section.sectionType]}</DialogTitle>
+                <DialogContent>{text[props.section.sectionType]}</DialogContent>
+                <Stack direction="row-reverse" spacing={2}>
+                    <Button
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            props.handleDelete();
+                            props.setOpen(false);
+                        }}
+                        startDecorator={<DeleteForeverOutlined />}
+                        color="danger"
+                        variant="solid"
+                    >
+                        Удалить
+                    </Button>
+                    <Button
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            props.setOpen(false);
+                        }}
+                        color="danger"
+                        variant="outlined"
+                    >
+                        Отмена
+                    </Button>
+                </Stack>
+            </ModalDialog>
+        </Modal>
+    );
+};
+
+export default DeleteSectionDialog;
