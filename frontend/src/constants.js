25a5c656457968ea106e6cd8e9f50d7b40e682b9
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-10-13 09:26:07 -0300

    Add a new file with constants

---
+++ frontend/src/constants.js
@@ -0,0 +1,26 @@
+export const FlightTypes = {
+    CUP: "Кубковое соревнование",
+    COMPETITION: "Соревнование",
+    TRAINING: "Тренировка",
+    JUNIOR_COMPETITION: "Юниорское соревнование"
+}
+
+export const afterFlightCondition = {
+    NORMAL: "Нормальное",
+    TIRED: "Уставший"
+}
+
+export const Sex = {
+    MALE: "самец",
+    FEMALE: "самка"
+}
+
+export const PIGEONS_URL = '/api/v1/pigeons';
+export const FLIGHTS_URL = "/api/v1/flights";
+export const KEEPER_URL = '/api/v1/keepers';
+export const MAIN_KEEPER_URL = KEEPER_URL + '/main';
+export const LAUNCH_POINTS_URL = "/api/v1/launch-point";
+export const HIERARCHICAL_SECTIONS_URL = "/api/v1/sections/hierarchical";
+export const SECTIONS_URL = "/api/v1/sections";
+export const HIERARCHICAL_SECTIONS_WITH_PIGEONS_URL = SECTIONS_URL + "/hierarchical-with-pigeons";
+
