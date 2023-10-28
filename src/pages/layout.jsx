54308792ebdcbcb8cca95bc8af1f2216ee8bc336
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-10-27 21:25:48 -0300

    Add jwt expiration check

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -4,11 +4,20 @@ import Header from "../components/UI/Header";
 import Footer from "../components/UI/Footer";
 
 const Layout = () => {
-    const isAuthenticated = Boolean(localStorage.getItem("auth_token"));
+    const isAuthenticated = () => {
+        const token = localStorage.getItem("auth_token");
+        if (!token) return false;
+
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace('-', '+').replace('_', '/');
+        const payload = JSON.parse(window.atob(base64));
+
+        return Date.now() <= payload.exp * 1000;
+    }
 
     return (
         <React.Fragment>
-            {isAuthenticated
+            {isAuthenticated()
                 ?
                 <React.Fragment>
                     <Header/>
