dad6e6d0bf1481db3aded4174789ff270bd1b638
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-10-29 23:16:06 -0300

    Fix display of footer

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import logoWhite from "../../images/pigeoner_logo_white.svg";
-import Container from "@mui/material/Container";
 import {Stack} from "@mui/material";
+import {Container} from "react-bootstrap";
+import {Link, Typography} from "@mui/joy";
 
 const Footer = () => {
     return (
         <footer className="pigeoner-footer">
             <Container>
-                <Stack direction="row" spacing={2} marginY={2} alignItems="center">
+                <Stack direction="row" spacing={2} marginY={2} justifyContent="space-between" alignItems="center">
                     <img src={logoWhite} alt="Pigeoner Logo white"/>
-                    <p>Разработка <a href={"http://filatov.tech/"}>filatov.tech</a></p>
+                    <Typography sx={{color: "#e7e7e7"}}>
+                        Разработка <Link href="https://filatov.tech/" textColor="#A2B3FFFF" underline="none">
+                            filatov.tech
+                        </Link>
+                    </Typography>
+
                 </Stack>
             </Container>
-
-
         </footer>
     );
 };
